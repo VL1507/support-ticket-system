13484066dfca373d36b0cdfea9de0e1d25c54efb
VL1507 <vova.mashyanov@gmail.com>
2026-01-14 19:51:03 +1000

    correct the typo "Tiket" to "Ticket"

--- init-db/01-create-tables.sql
+++ init-db/01-create-tables.sql
@@ -16,16 +16,16 @@ CREATE TABLE IF NOT EXISTS "Role" (
 );
 
 
-CREATE TABLE IF NOT EXISTS "Tiket" (
+CREATE TABLE IF NOT EXISTS "Ticket" (
 	"id" serial NOT NULL UNIQUE,
-	"tiket_status_id" int NOT NULL,
+	"ticket_status_id" int NOT NULL,
 	"user_id" int NOT NULL,
 	"created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
 	PRIMARY KEY("id")
 );
 
 
-CREATE TABLE IF NOT EXISTS "TiketStatus" (
+CREATE TABLE IF NOT EXISTS "TicketStatus" (
 	"id" serial NOT NULL UNIQUE,
 	"name" varchar(255) NOT NULL UNIQUE,
 	PRIMARY KEY("id")
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS "TiketStatus" (
 
 CREATE TABLE IF NOT EXISTS "Message" (
 	"id" serial NOT NULL UNIQUE,
-	"tiket_id" int NOT NULL,
+	"ticket_id" int NOT NULL,
 	"text" text NOT NULL,
 	"created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
 	"user_id" int NOT NULL,
@@ -45,14 +45,14 @@ CREATE TABLE IF NOT EXISTS "Message" (
 ALTER TABLE "User"
 ADD FOREIGN KEY("role_id") REFERENCES "Role"("id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE "Tiket"
-ADD FOREIGN KEY("tiket_status_id") REFERENCES "TiketStatus"("id")
+ALTER TABLE "Ticket"
+ADD FOREIGN KEY("ticket_status_id") REFERENCES "TicketStatus"("id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE "Tiket"
+ALTER TABLE "Ticket"
 ADD FOREIGN KEY("user_id") REFERENCES "User"("id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE "Message"
-ADD FOREIGN KEY("tiket_id") REFERENCES "Tiket"("id")
+ADD FOREIGN KEY("ticket_id") REFERENCES "Ticket"("id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE "Message"
 ADD FOREIGN KEY("user_id") REFERENCES "User"("id")
