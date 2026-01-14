8b39b3c4594e7d682270cc4b7aa86472fd209594
VL1507 <vova.mashyanov@gmail.com>
2026-01-14 20:46:04 +1000

    add TicketStatusHistory update Ticket

--- init-db/01-create-tables.sql
+++ init-db/01-create-tables.sql
@@ -18,16 +18,27 @@ CREATE TABLE IF NOT EXISTS "Role" (
 
 CREATE TABLE IF NOT EXISTS "Ticket" (
 	"id" serial NOT NULL UNIQUE,
-	"ticket_status_id" int NOT NULL,
+	"title" varchar(255) NOT NULL,
 	"user_id" int NOT NULL,
+	"ticket_status_id" int NOT NULL,
+	"ticket_category_id" int NOT NULL,
 	"created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+	"updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+	"resolution_text" varchar(255),
+	"closed_at" TIMESTAMPTZ,
 	PRIMARY KEY("id")
 );
 
-
+CREATE INDEX "Ticket_index_0"
+ON "Ticket" ("user_id");
+CREATE INDEX "Ticket_index_1"
+ON "Ticket" ("created_at");
+CREATE INDEX "Ticket_index_2"
+ON "Ticket" ("updated_at");
 CREATE TABLE IF NOT EXISTS "TicketStatus" (
 	"id" serial NOT NULL UNIQUE,
 	"name" varchar(255) NOT NULL UNIQUE,
+	"is_closed" boolean NOT NULL DEFAULT false,
 	PRIMARY KEY("id")
 );
 
@@ -38,6 +49,28 @@ CREATE TABLE IF NOT EXISTS "Message" (
 	"text" text NOT NULL,
 	"created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
 	"user_id" int NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE INDEX "Message_index_0"
+ON "Message" ("ticket_id");
+CREATE INDEX "Message_index_1"
+ON "Message" ("created_at");
+CREATE TABLE IF NOT EXISTS "TicketCategory" (
+	"id" serial NOT NULL UNIQUE,
+	"name" varchar(255) NOT NULL,
+	PRIMARY KEY("id")
+);
+
+
+CREATE TABLE IF NOT EXISTS "TicketStatusHistory" (
+	"id" serial NOT NULL UNIQUE,
+	"ticket_id" int NOT NULL,
+	"from_status_id" int NOT NULL,
+	"to_status_id" int NOT NULL,
+	"changed_by_user_id" int NOT NULL,
+	"changed_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+	"comment" text,
 	PRIMARY KEY("id")
 );
 
@@ -56,4 +89,19 @@ ADD FOREIGN KEY("ticket_id") REFERENCES "Ticket"("id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 ALTER TABLE "Message"
 ADD FOREIGN KEY("user_id") REFERENCES "User"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "Ticket"
+ADD FOREIGN KEY("ticket_category_id") REFERENCES "TicketCategory"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "TicketStatusHistory"
+ADD FOREIGN KEY("ticket_id") REFERENCES "Ticket"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "TicketStatusHistory"
+ADD FOREIGN KEY("from_status_id") REFERENCES "TicketStatus"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "TicketStatusHistory"
+ADD FOREIGN KEY("to_status_id") REFERENCES "TicketStatus"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "TicketStatusHistory"
+ADD FOREIGN KEY("changed_by_user_id") REFERENCES "User"("id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
