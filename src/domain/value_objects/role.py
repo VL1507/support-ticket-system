2fb475e7651af5f3c8271e13ce8ad3fa22a9073b
VL1507 <vova.mashyanov@gmail.com>
2026-01-16 14:25:46 +1000

    edit role

--- src/domain/value_objects/role.py
+++ src/domain/value_objects/role.py
@@ -17,25 +17,3 @@ class Role(StrEnum):
     @property
     def is_user(self) -> bool:
         return self is Role.USER
-
-
-# from dataclasses import dataclass
-# from typing import Literal
-
-
-# @dataclass(frozen=True)
-# class Role:
-# id: int
-# name: Literal["user", "support", "admin"]
-
-# @property
-# def is_admin(self) -> bool:
-#     return self.name == "admin"
-
-# @property
-# def is_support(self) -> bool:
-#     return self.name == "support"
-
-# @property
-# def is_user(self) -> bool:
-#     return self.name == "user"
