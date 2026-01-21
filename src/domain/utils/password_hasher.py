09c6efba8f5ea9d75d40477dd4795fc3a8a23352
VL1507 <vova.mashyanov@gmail.com>
2026-01-21 20:55:15 +1000

    add abstractmethod for protect

--- src/domain/utils/password_hasher.py
+++ src/domain/utils/password_hasher.py
@@ -1,9 +1,14 @@
+from abc import abstractmethod
 from typing import Protocol
 
 
 class IPasswordHasher(Protocol):
-    def hash_password(self, plain_password: str) -> str: ...
+    @abstractmethod
+    def hash_password(self, plain_password: str) -> str:
+        pass
 
+    @abstractmethod
     def verify_password(
         self, plain_password: str, hashed_password: str
-    ) -> bool: ...
+    ) -> bool:
+        pass
